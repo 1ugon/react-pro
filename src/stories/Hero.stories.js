fe7c28c64fcad71513ed4506244760ac5caa1698
Pedro Motta <pedromotta20@gmail.com>
2020-11-24 23:45:08 -0300

    Let's get it started

---
+++ src/stories/Hero.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Hero from "../components/Hero";
+
+export default {
+  title: "Hero",
+  component: Hero,
+};
+
+export const usage = () => (
+  <Hero title="Ganhe sua liberdade para ir e vir">
+    <p>A auto escola lidar em aprovacao</p>
+  </Hero>
+);
