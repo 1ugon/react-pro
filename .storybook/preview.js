dfc401e23d9d14a6e3d0071e68b626eb12939ad7
Pedro Motta <pedromotta20@gmail.com>
2020-11-26 01:05:51 -0300

    Aprendendo storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import React from "react";
+import { addDecorator } from "@storybook/react";
+
+import GlobalStyle from "../src/components/GlobalStyle";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+addDecorator((storyFn) => (
+  <>
+    <GlobalStyle />
+    {storyFn()}
+  </>
+));
--- src/stories/Hero.stories.js
+++ src/stories/Hero.stories.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 import Hero from "../components/Hero";
+import Cuba from "./assets/cuba.jpg";
+import Volks from "./assets/volks.png";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   title: "Hero",
   component: Hero,
 };
 
 export const usage = () => (
-  <Hero title="Ganhe sua liberdade para ir e vir">
-    <p>A auto escola lidar em aprovacao</p>
+  <Hero title="Ganhe sua liberdade para ir e vir" image={Volks}>
+    <ul>
+      <li>A auto escola lider em aprovacao</li>
+      <li>A auto escola lider em aprovacao 2</li>
+      <li>A auto escola lider em aprovacao 3</li>
+    </ul>
   </Hero>
 );
+
+export const withList = () => <Hero title="TESTE" image={Cuba}></Hero>;
