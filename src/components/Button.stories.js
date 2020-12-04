4e503428e9415a3ba67a28854328a44621865acb
Pedro Motta <pedromotta20@gmail.com>
2020-12-04 12:29:02 -0300

    Buttons

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,10 +1,102 @@
 import React from "react";
+import { actions } from "@storybook/addon-actions";
 import Button from "./Button";
-import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
 
 export default {
   title: "Components/Button",
   component: Button,
 };
 
-export const usage = () => <Button>Meu botao</Button>;
+const events = actions({ onClick: "clicked" });
+
+const Toolbar = styled.div`
+  & > * {
+    margin-right: 8px;
+  }
+`;
+
+export const usage = () => (
+  <div>
+    <p>Enabed:</p>
+    <Toolbar>
+      <Button {...events}>Default</Button>
+      <Button color="primary" {...events}>
+        Primary
+      </Button>
+      <Button color="danger" {...events}>
+        Danger
+      </Button>
+    </Toolbar>
+    <p>Disabled:</p>
+    <Toolbar>
+      <Button disabled {...events}>
+        Default
+      </Button>
+      <Button color="primary" disabled {...events}>
+        Primary
+      </Button>
+      <Button color="danger" disabled {...events}>
+        Danger
+      </Button>
+    </Toolbar>
+  </div>
+);
+
+export const outlined = () => (
+  <div>
+    <p>Enabed:</p>
+    <Toolbar>
+      <Button variant="outlined" {...events}>
+        Default
+      </Button>
+      <Button variant="outlined" color="primary" {...events}>
+        Primary
+      </Button>
+      <Button variant="outlined" color="danger" {...events}>
+        Danger
+      </Button>
+    </Toolbar>
+    <p>Disabled:</p>
+    <Toolbar>
+      <Button variant="outlined" disabled {...events}>
+        Default
+      </Button>
+      <Button variant="outlined" color="primary" disabled {...events}>
+        Primary
+      </Button>
+      <Button variant="outlined" color="danger" disabled {...events}>
+        Danger
+      </Button>
+    </Toolbar>
+  </div>
+);
+
+export const link = () => (
+  <div>
+    <p>Enabed:</p>
+    <Toolbar>
+      <Button variant="link" {...events}>
+        Default
+      </Button>
+      <Button variant="link" color="primary" {...events}>
+        Primary
+      </Button>
+      <Button variant="link" color="danger" {...events}>
+        Danger
+      </Button>
+    </Toolbar>
+    <p>Disabled:</p>
+    <Toolbar>
+      <Button variant="link" disabled {...events}>
+        Default
+      </Button>
+      <Button variant="link" color="primary" disabled {...events}>
+        Primary
+      </Button>
+      <Button variant="link" color="danger" disabled {...events}>
+        Danger
+      </Button>
+    </Toolbar>
+  </div>
+);
