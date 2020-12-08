07b8c477291eaa2f7af81fd282d530265b9a4244
Pedro Motta <pedromotta20@gmail.com>
2020-12-08 18:54:57 -0300

    Ending tests

--- src/components/atoms/Feature.stories.js
+++ src/components/atoms/Feature.stories.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { FaCar } from "react-icons/fa";
+import { FaCar, FaKey, FaMapMarkedAlt, FaAccessibleIcon } from "react-icons/fa";
 
 import Feature from "./Feature";
+import Grid from "./Grid";
 
 export default {
   title: "Components/Atoms/Feature",
@@ -12,4 +13,21 @@ export const usage = () => (
   <Feature icon={<FaCar />} title="Titulo">
     <p>Algum texto</p>
   </Feature>
+);
+
+export const withGrid = () => (
+  <Grid sm={2} md={4}>
+    <Feature icon={<FaCar />} title="Maior e melhor frota">
+      <p>Algum texto</p>
+    </Feature>
+    <Feature icon={<FaKey />} title="Habilitacao mais rapida">
+      <p>Algum texto</p>
+    </Feature>
+    <Feature icon={<FaMapMarkedAlt />} title="Excelente localizacao">
+      <p>Algum texto</p>
+    </Feature>
+    <Feature icon={<FaAccessibleIcon />} title="Acessibilidade">
+      <p>Algum texto</p>
+    </Feature>
+  </Grid>
 );
